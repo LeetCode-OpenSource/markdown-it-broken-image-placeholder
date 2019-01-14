023ba0be54abb93862a052f4de6e35948f4631aa
Runjuu <huang@runjuu.com>
2019-01-14 18:04:25 +0800

    feat: populate initial code

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import MarkdownIt = require('markdown-it');
+
+interface Config {
+  fallbackLink: string;
+}
+
+export function brokenImagePlaceholderPlugin(config: Config) {
+  return (md: MarkdownIt) => {
+    const defaultRender = md.renderer.rules.image;
+
+    md.renderer.rules.image = function(tokens, index, options, env, self) {
+      const token = tokens[index];
+      const onError = token.attrGet('onerror') || '';
+
+      token.attrSet('onerror', `this.src='${config.fallbackLink}'; ${onError}`);
+
+      return defaultRender(tokens, index, options, env, self);
+    };
+  };
+}
